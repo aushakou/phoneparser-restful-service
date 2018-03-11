a262e0ae1cb7c3dfb7353743a7fadabf9276eac4
Aliaksandr Ushakou <ushakou.aleksandr@gmail.com>
2018-03-11 18:59:40 -0400

    Unit tests changed back to synchronous
    Async testing doesn't work

--- app.test.js
+++ app.test.js
@@ -11,33 +11,27 @@ const getUrlWithoutNumber = '/api/phonenumbers/parse/text/';
 const getUrlWithURL = '/api/phonenumbers/url/http/?url=http://www.senecacollege.ca/contact';
 
 describe('Testing GET requests:', () => {
-  test('GET request with empty URL', (done) => {
+  test('GET request with empty URL', () =>
     request(address).get(getUrlWithoutNumber).then((response) => {
       expect(response.statusCode).toBe(200);
       expect(response.text).toBe('[]');
-      done();
-    });
-  });
+    }));
 
-  test('GET request with URL containing valid numbers', (done) => {
+  test('GET request with URL containing valid numbers', () =>
     request(address).get(getUrlWithNumber).then((response) => {
       expect(response.statusCode).toBe(200);
       expect(response.text).toBe('["(647) 472-7272","(647) 569-5656"]');
-      done();
-    });
-  });
+    }));
 
-  test('GET request with URL containing URL of the resource', (done) => {
+  test('GET request with URL containing URL of the resource', () =>
     request(address).get(getUrlWithURL).then((response) => {
       expect(response.statusCode).toBe(200);
       expect(response.text).toBe('["(416) 491-5050","(416) 491-8811","(905) 833-1650"]');
-      done();
-    });
-  });
+    }));
 });
 
 describe('Testing POST requests:', () => {
-  test('POST request with file containing valid numbers', (done) => {
+  test('POST request with file containing valid numbers', () =>
     request(address)
       .post(postURL)
       .set('Content-Type', 'text/plain')
@@ -45,11 +39,9 @@ describe('Testing POST requests:', () => {
       .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe('["(647) 472-2593","(647) 425-5696","(867) 425-6565"]');
-        done();
-      });
-  });
+      }));
 
-  test('POST request with file containing invalid numbers', (done) => {
+  test('POST request with file containing invalid numbers', () =>
     request(address)
       .post(postURL)
       .set('Content-Type', 'text/plain')
@@ -57,11 +49,9 @@ describe('Testing POST requests:', () => {
       .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe('[]');
-        done();
-      });
-  });
+      }));
 
-  test('POST request with MS Word file containing valid numbers', (done) => {
+  test('POST request with MS Word file containing valid numbers', () =>
     request(address)
       .post(postURLDoc)
       .set('Content-Type', 'application/octet-stream')
@@ -69,40 +59,41 @@ describe('Testing POST requests:', () => {
       .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe('["(416) 491-5050","(416) 491-8811","(905) 833-1650"]');
-        done();
-      });
-  });
+      }));
 
-  test('POST request with image 1 containing valid numbers', (done) => {
-    request(address)
+  test('POST request with image 1 containing valid numbers', () => {
+    // Asynchronous testing should be implemented
+    jest.setTimeout(20000);
+    return request(address)
       .post(postUrlImage)
       .attach('file', fs.readFileSync('./images-for-testing/test_image_seneca_contacts.jpg'), 'testImage.jpg')
       .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe('["(416) 491-5050","(416) 491-8811","(905) 833-1650"]');
-        done();
       });
   });
 
-  test('POST request with image 2 containing valid numbers', (done) => {
-    request(address)
+  test('POST request with image 2 containing valid numbers', () => {
+    // Asynchronous testing should be implemented
+    jest.setTimeout(20000);
+    return request(address)
       .post(postUrlImage)
       .attach('file', fs.readFileSync('./images-for-testing/test_image_ctv_contacts.jpg'), 'testImage.jpg')
       .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe('["(416) 384-5000","(866) 690-6179","(800) 668-0060","(800) 461-1542"]');
-        done();
       });
   });
 
-  test('POST request with image 3 containing valid numbers', (done) => {
-    request(address)
+  test('POST request with image 3 containing valid numbers', () => {
+    // Asynchronous testing should be implemented
+    jest.setTimeout(20000);
+    return request(address)
       .post(postUrlImage)
       .attach('file', fs.readFileSync('./images-for-testing/test_image_toronto_zoo_contacts.jpg'), 'testImage.jpg')
       .then((response) => {
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe('["(416) 392-5900","(416) 392-5934","(416) 392-9114","(416) 392-9115","(416) 392-5962","(416) 392-4979","(416) 392-5940","(416) 392-5863","(416) 392-5947","(416) 392-5948","(416) 392-5932","(416) 392-5944","(416) 392-5924","(416) 393-6364","(416) 392-5929","(416) 393-6339","(416) 392-5905","(416) 392-9101"]');
-        done();
       });
   });
 });
