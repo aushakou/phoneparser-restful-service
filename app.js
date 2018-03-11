ea9c216d3cb19561feba27a42b0a2c17ff721d39
Aliaksandr Ushakou <ushakou.aleksandr@gmail.com>
2018-03-11 18:57:46 -0400

    app.js updated
    Fixed all Eslint errors and warnings in app.js

--- app.js
+++ app.js
@@ -5,51 +5,51 @@ const fs = require('fs');
 const request = require('request');
 const validUrl = require('valid-url');
 const mammoth = require('mammoth');
+const path = require('path');
+const tesseractjs = require('tesseract.js');
+
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, '.');
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     cb(null, 'file-to-be-parsed');
   },
 });
-const upload = multer({ storage: storage });
-const path = require("path");
-const tesseractjs = require('tesseract.js');
+const upload = multer({ storage });
+
 const Tesseract = tesseractjs.create({
   workerPath: path.join(__dirname, './node_modules/tesseract.js/src/node/worker.js'),
   langPath: path.join('./eng.traineddata'),
-  corePath: path.join(__dirname, './node_modules/tesseract.js/src/index.js')
+  corePath: path.join(__dirname, './node_modules/tesseract.js/src/index.js'),
 });
 
 const app = express();
 
 function isEmpty(obj) {
-  for (let prop in obj) {
-    if (obj.hasOwnProperty(prop)) {
-      return false;
-    }
+  if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+    return true;
   }
-  return true;
+  return false;
 }
 
 function phoneNumberParser(data) {
-  const regexp = /[^\d\s-()\.\+]/;
-  let resultArray = [];
+  const regexp = /[^\d\s-().+]/;
+  const arrayOfStrings = data.split(regexp);
+  const resultArray = [];
   let phoneNumber;
   let checkDuplicate = false;
-  let arrayOfStrings = data.split(regexp);
   for (let i = 0; i < arrayOfStrings.length; i += 1) {
     phoneNumber = libphonenumberModule.parse(arrayOfStrings[i], 'CA');
     if (!isEmpty(phoneNumber)) {
-      let formattedPhoneNumber = libphonenumberModule.format({ country: 'CA', phone: phoneNumber.phone }, 'National');
+      const formattedPhoneNumber = libphonenumberModule.format({ country: 'CA', phone: phoneNumber.phone }, 'National');
       for (let j = 0; j < resultArray.length; j += 1) {
         if (resultArray[j] === formattedPhoneNumber) {
           checkDuplicate = true;
           break;
         }
       }
-      let isNumberValid = libphonenumberModule.isValidNumber(phoneNumber);
+      const isNumberValid = libphonenumberModule.isValidNumber(phoneNumber);
       if (checkDuplicate === false && isNumberValid === true) {
         resultArray.push(formattedPhoneNumber);
       }
@@ -59,14 +59,15 @@ function phoneNumberParser(data) {
   return resultArray;
 }
 
-app.get('/api/phonenumbers/url/http/', function (req, res) {
-  let url = req.query.url;
+app.get('/api/phonenumbers/url/http/', (req, res) => {
+  // Object destructuring
+  const { url } = req.query;
   let result;
   if (!validUrl.isWebUri(url)) {
     res.status(400).end();
   }
-  request({ uri: url }, function (error, response, body) {
-    if (error || response.statusCode != 200) {
+  request({ uri: url }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
       res.status(400).end();
     } else {
       result = phoneNumberParser(body);
@@ -75,88 +76,72 @@ app.get('/api/phonenumbers/url/http/', function (req, res) {
   });
 });
 
-app.get('/api/phonenumbers/parse/text/:string', function (req, res) {
-  let result = phoneNumberParser(req.params.string);
+app.get('/api/phonenumbers/parse/text/:string', (req, res) => {
+  const result = phoneNumberParser(req.params.string);
   res.status(200).json(result);
 });
 
-app.get('/api/phonenumbers/parse/text/', function (req, res) {
-  let result = [];
+app.get('/api/phonenumbers/parse/text/', (req, res) => {
+  const result = [];
   res.status(200).json(result);
 });
 
-app.post('/api/phonenumbers/parse/image', upload.single('file'), function (req, res) {
+app.post('/api/phonenumbers/parse/image', upload.single('file'), (req, res) => {
   if (!req.file) {
     // No file attached
     res.status(400).end();
   } else {
-    var fileType = (req.file.mimetype).split('/');
+    const fileType = (req.file.mimetype).split('/');
     if (fileType[0] !== 'image') {
       // Uploaded file is not an image
       res.status(400).end();
     } else {
       Tesseract.recognize(req.file.path)
-        .then(function (result) { res.status(200).json(phoneNumberParser(result.text)); })
+        .then((result) => { res.status(200).json(phoneNumberParser(result.text)); });
     }
     // Deleting uploaded file
-    // May be this function should be invoked before the response
-    fs.unlink(req.file.path, (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-    });
+    fs.unlinkSync(req.file.path);
   }
 });
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   // Invalid URL
   res.status(404).end();
 });
 
-app.post('/api/phonenumbers/parse/file', upload.single('file'), function (req, res) {
+app.post('/api/phonenumbers/parse/file', upload.single('file'), (req, res) => {
   if (!req.file) {
     // No file received
-    console.log('file not received');
     res.status(400).end();
   } else {
     fs.readFile(req.file.path, 'utf8', (err, data) => {
       if (err) throw err;
-      let fileContent = Buffer.from(data, 'base64').toString();
-      let result = phoneNumberParser(fileContent);
+      const fileContent = Buffer.from(data, 'base64').toString();
+      const result = phoneNumberParser(fileContent);
       res.status(200).json(result);
     });
     // Deleting uploaded file
-    // May be this function should be invoked before the response
-    fs.unlink(req.file.path, (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-    });
+    fs.unlinkSync(req.file.path);
   }
 });
 
-app.post('/api/phonenumbers/parse/doc', upload.single('file'), function (req, res) {
+app.post('/api/phonenumbers/parse/doc', upload.single('file'), (req, res) => {
   if (!req.file) {
     // No file received
     res.status(400).end();
   } else {
     mammoth.extractRawText({ path: req.file.path }).then((data) => {
-      let result = phoneNumberParser(data.value);
+      const result = phoneNumberParser(data.value);
       res.status(200).json(result);
     });
     // Deleting uploaded file
-    // May be this function should be invoked before the response
-    fs.unlink(req.file.path, (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-    });
+    fs.unlinkSync(req.file.path);
   }
 });
 
-app.post('*', function (req, res) {
+app.post('*', (req, res) => {
   // Invalid URL
   res.status(404).end();
 });
 
-app.listen(3000, () => console.log('Server started on port 3000...'));
+app.listen(3000);
